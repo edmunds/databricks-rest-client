38d8baf907b6f21764de94f02f1c1fc409f60f98
samshuster <samshuste@gmail.com>
2018-09-19 08:01:08 -0700

    adding to README adding more javadoc to ClusterService

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
+# Databricks Java Rest Client
+
+_This is a simple java library that provides programmatic access to the [Databricks Rest Service](https://docs.databricks.com/api/latest/index.html)._
 
 
-# Integration Tests
-## Setup
+## API Overview
+
+[![Javadocs](http://www.javadoc.io/badge/com.edmunds.databricks/databricks-rest-client.svg)](http://www.javadoc.io/doc/com.edmunds.databricks/databricks-rest-client)
+
+This library only implements a percentage of all of the functionality that the Databricks Rest Interface provides.
+The idea is to add functionality as users of this library need it. Below you will find a quick summary of 
+the current supported functionality with a brief example. For details on exactly which methods are supported,
+please see the accompanying java doc.
+
+###Cluster Service
+
+The cluster service allows 
+
+## Examples
+
+## Building, Installing and Running
+
+### Getting Started and Prerequisites
+
+- You will need Maven installed
+
+### Building
+
+*How to build the project locally:*
+```mvn clean install```
+
+
+## Unit Tests
+
+There are currently no unit tests for this project. Our thoughts are that the only testable
+functionality is the integration between our client on an actual databricks instance.
+As such we currently only have integration tests.
+
+
+## Integration Tests
+### Setup
 You need to set the following environment properties in your .bash_profile
 ```bash
 export DB_USER=myuser@domain.com
@@ -14,6 +51,14 @@ In order for the integration tests to run, you must
 have a valid token for the user in question.
 Here is how to set it up: [Set up Tokens](https://docs.databricks.com/api/latest/authentication.html)
 
-## Execution
+### Executing Integration Tests
 mvn clean install org.apache.maven.plugins:maven-failsafe-plugin:integration-test
 
+## Deployment
+
+Please see the CONTRIBUTING.md about our release process.
+As this is a library, there is no deployment operation needed.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for the process for merging code into master.
--- src/main/java/com/edmunds/rest/databricks/service/ClusterService.java
+++ src/main/java/com/edmunds/rest/databricks/service/ClusterService.java
@@ -29,23 +29,89 @@ import java.io.IOException;
  */
 public interface ClusterService {
 
+  /**
+   * Creates a databricks cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#create
+   * @param createClusterRequest the cluster request object
+   * @return the clusterId
+   * @throws IOException other connection errors
+   * @throws DatabricksRestException any errors with the request
+   */
   String create(CreateClusterRequest createClusterRequest)
       throws IOException, DatabricksRestException;
 
+  /**
+   * Edits the configurations of a databricks cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#edit
+   * @param editClusterRequest the edit cluster request object
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with the request
+   */
   void edit(EditClusterRequest editClusterRequest) throws IOException, DatabricksRestException;
 
+  /**
+   * Starts a databricks cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#start
+   * @param clusterId the clusterId of the cluster you want to start.
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with the request
+   */
   void start(String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Restarts a given databricks cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#restart
+   * @param clusterId the clusterid of the cluster you want to restart.
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with the request
+   */
   void restart(String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Resizes a cluster. Will give it a fixed number of workers.
+   * https://docs.databricks.com/api/latest/clusters.html#resize
+   * @param numWorkers the number of workers you want to resize to
+   * @param clusterId the clusterId you want to operate on
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with request
+   */
   void resize(int numWorkers, String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Resizes a cluster. Will add/edit autoscale functionality.
+   * https://docs.databricks.com/api/latest/clusters.html#resize
+   * @param autoscale the autoscale specs.
+   * @param clusterId the clusterId you want to work on
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with request
+   */
   void resize(AutoScaleDTO autoscale, String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Terminates a cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#delete-terminate
+   * @param clusterId the cluster you want to terminate
+   * @throws IOException any other errors
+   * @throws DatabricksRestException errors with request
+   */
   void delete(String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Gets information about a given cluster.
+   * https://docs.databricks.com/api/latest/clusters.html#get
+   * @param clusterId the cluster you want to get info about
+   * @return the information DTO object
+   * @throws IOException any other errors.
+   * @throws DatabricksRestException any errors with the request
+   */
   ClusterInfoDTO getInfo(String clusterId) throws IOException, DatabricksRestException;
 
+  /**
+   * Lists all of the clusters on a given databricks instance.
+   * @return an array of cluster information objects
+   * @throws IOException any other errors
+   * @throws DatabricksRestException any errors with the request
+   */
   ClusterInfoDTO[] list() throws IOException, DatabricksRestException;
 
 }
