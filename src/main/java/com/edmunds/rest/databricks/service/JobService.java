6f874376213981cb657c8152fb66b9dc5f70ed77
Jyothi Kommajosyula <jkommajosyula@foursquare.com>
2019-08-01 12:35:13 -0400

    DP-1713: Added support for additional REST methods

--- src/main/java/com/edmunds/rest/databricks/service/JobService.java
+++ src/main/java/com/edmunds/rest/databricks/service/JobService.java
@@ -204,4 +204,19 @@ public interface JobService {
   void upsertJob(JobSettingsDTO jobSettingsDTO, boolean failOnDuplicateJobNames)
       throws IOException, DatabricksRestException;
 
+  /**
+   * Submit a one-time run. This endpoint doesn’t require a Databricks job to be created
+   * @see <a href="https://docs.databricks.com/api/latest/jobs.html#runs-submit">https://docs.databricks.com/api/latest/jobs.html#runs-submit</a>
+   * @param jobSettings the settings to change the job to
+   * @return Returns the run_id of the triggered run
+   */
+  RunNowDTO runSubmit(JobSettingsDTO jobSettings) throws IOException, DatabricksRestException;
+
+  /**
+   * Retrieve the output of a run
+   * @see <a href="https://docs.databricks.com/api/latest/jobs.html#runs-get-output">https://docs.databricks.com/api/latest/jobs.html#runs-get-output</a>
+   * @param runId The desired run id
+   * @return Returns the data output of the specified run
+   */
+  String getOutput(long runId) throws IOException, DatabricksRestException;
 }
