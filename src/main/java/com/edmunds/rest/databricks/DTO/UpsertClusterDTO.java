b13d5cbe768087d81ad5a55feca0a72d16401927
Denys Serdiuk <dserdiuk@bpcs.com>
2022-04-04 16:57:19 -0700

    fix clusters/edit api

--- src/main/java/com/edmunds/rest/databricks/DTO/UpsertClusterDTO.java
+++ src/main/java/com/edmunds/rest/databricks/DTO/UpsertClusterDTO.java
@@ -1,6 +1,6 @@
 package com.edmunds.rest.databricks.DTO;
 
-import com.edmunds.rest.databricks.DTO.jobs.NewClusterDTO;
+import com.edmunds.rest.databricks.DTO.clusters.ClusterInfoDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import lombok.Data;
@@ -9,7 +9,7 @@ import lombok.Data;
  *
  */
 @Data
-public class UpsertClusterDTO extends NewClusterDTO implements Serializable {
+public class UpsertClusterDTO extends ClusterInfoDTO implements Serializable {
 
   @JsonProperty("cluster_id")
   private String clusterId;
