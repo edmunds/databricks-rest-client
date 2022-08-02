f8012436601013f8f108b281f9be29edfde06dc4
Doru Chiulan <doru.chiulan@gmail.com>
2022-08-02 11:10:48 +0300

    Add runType

--- src/main/java/com/edmunds/rest/databricks/service/JobService.java
+++ src/main/java/com/edmunds/rest/databricks/service/JobService.java
@@ -152,12 +152,13 @@ public interface JobService {
    *     default value is 20
    * @param limit The number of runs to return. This value should be greater than 0 and less than
    *     1000
+   * @param runType JOB_RUN, SUBMIT_RUN, WORKFLOW_RUN
    * @param from Show runs that started at or after this value. The value must be a UTC timestamp in milliseconds
    * @param until Show runs that started at or before this value. The value must be a UTC timestamp in milliseconds
    * @return Returns RunsDTO containing an array of runs and a boolean indicating if there are more
    *     jobs that haven't been included
    */
-  RunsDTO listRuns(Long jobId, Boolean activeOnly, Integer offset, Integer limit, Long from, Long until)
+  RunsDTO listRuns(Long jobId, Boolean activeOnly, Integer offset, Integer limit, Long from, Long until, String runType)
       throws DatabricksRestException,
       IOException;
 
